fdce4187974263410475af24491bf373deb96686
CJ Metcalfe <khaaiev@gmail.com>
2024-02-05 14:02:20 -0500

    SD10 Major Update - 240105

--- CLI/MenuCli.cs
+++ CLI/MenuCli.cs
@@ -10,17 +10,7 @@ namespace AngelHornetLibrary.CLI
 {
     public class MenuCli
     {
-        public Action? DefaultAction { get; set; } = null;
-        public bool UseNumbers { get; private set; } = true;  // Use Numbers for Menu Items ... will fix this later
-        public bool PadMenuItems { get; set; } = false;        // Pad Menu Items to MenuItemWidth   
-        public int MenuItemWidth { get; set; } = 14;        // 6 menu items vs Standard old VT100 Terminal Width
-        public int MenuMaxWidth { get; set; } = 90;         // Standard old VT100 Terminal Width
-        public bool RunLoop { get; set; } = false;
-
-        // Debug and Warning Variables
-        static bool warnAddItem = false;
-
-        struct MenuItem
+        public struct MenuItem
         {
             public List<String> commandStrings;
             public Action? actionOnSelect;
@@ -28,13 +18,37 @@ namespace AngelHornetLibrary.CLI
             public MenuItem(List<String> commandStrings, Action actionOnSelect) { this.commandStrings = commandStrings; this.actionOnSelect = actionOnSelect; }
         }
 
-        MenuItem[] menuItems = new MenuItem[0];
-        
+        public MenuItem[] menuItems = new MenuItem[0];
+
+        public Action? EntryAction { get; set; } = null;
+        public Action? ExitAction { get; set; } = null;
+        public Action? DefaultAction { get; set; } = null;
+
+        public bool UseNumbers { get; private set; } = true;  // Use Numbers for Menu Items ... will fix this later
+        public bool PadMenuItems { get; set; } = false;        // Pad Menu Items to MenuItemWidth   
+        public int MenuItemWidth { get; set; } = 14;        // 6 menu items vs Standard old VT100 Terminal Width
+        public int MenuMaxWidth { get; set; } = 90;         // Standard old VT100 Terminal Width
+        public bool RunLoop { get; set; } = false;
+        public string ErrorMsg { get; set; } = "";
 
 
+
+        public void AddOnEntry(Action actionOnSelect)
+        {
+            if (actionOnSelect == null) throw new ArgumentNullException("actionOnSelect");
+            EntryAction = actionOnSelect;
+        }
+        public void AddOnEntry(int index) => EntryAction = menuItems[index].actionOnSelect;
+        public void AddOnExit(Action actionOnSelect) => ExitAction = actionOnSelect;
+        public void AddOnExit(int index) => ExitAction = menuItems[index].actionOnSelect;
         public void AddDefault(Action actionOnSelect) => DefaultAction = actionOnSelect;
+        public void AddDefault(int index) => DefaultAction = menuItems[index].actionOnSelect;
+        public Action? GetEntryAction() => EntryAction;
+        public Action? GetExitAction() => ExitAction;
+        public Action? GetDefaultAction() => DefaultAction;
+        public Action? GetAction(int index) => menuItems[index].actionOnSelect;
 
-        // First Command is the Default Command if the DefaultAction is not set
+
         public void AddItem(string command, Action actionOnSelect) => AddItem(new List<string> { command }, actionOnSelect);
         public void AddItem(List<String> commands, Action actionOnSelect)
         {
@@ -45,7 +59,8 @@ namespace AngelHornetLibrary.CLI
 
         public void PrintMenu()
         {
-            Console.WriteLine();
+            Console.WriteLine(ErrorMsg);
+            ErrorMsg = "";
             String sum = "";
             for (int i = 0; i < menuItems.Length; i++)
             {
@@ -71,7 +86,6 @@ namespace AngelHornetLibrary.CLI
         // public Action Exit(bool T)
         public Action? Exit()
         {
-            Console.WriteLine("Exiting Menu");
             RunLoop = false;
             return null;
         }
@@ -84,11 +98,13 @@ namespace AngelHornetLibrary.CLI
             else if (menuItems.Length >= 1 && menuItems[0].actionOnSelect != null)
                 menuItems[0].actionOnSelect.Invoke();
         }
-        public Action? Loop() => Run();
-        public Action? Run()
+
+
+        public Action? Loop()
         {
-            if (!RunLoop) DefaultActionInvoke();
             RunLoop = true;
+
+            if (EntryAction != null) EntryAction.Invoke();
             while (RunLoop)
             {
                 PrintMenu();
@@ -98,19 +114,20 @@ namespace AngelHornetLibrary.CLI
                 else if (int.TryParse(input, out int index))
                 {
                     if (index > 0 && index <= menuItems.Length)
-                    {
                         menuItems[index - 1].actionOnSelect?.Invoke();
-                    }
                     else
                     {
-                        Console.WriteLine("Invalid Command:  Index out of Range");
+                        ErrorMsg = "Invalid Command:  Index out of Range";
+                        DefaultActionInvoke();
                     }
                 }
                 else
                 {
-                    Console.WriteLine("Invalid Command:  Failed to Parse Input");
+                    ErrorMsg = "Invalid Command:  Failed to Parse Input";
+                    DefaultActionInvoke();
                 }
             }
+            if (ExitAction != null) ExitAction.Invoke();
             return null;
         }
 
